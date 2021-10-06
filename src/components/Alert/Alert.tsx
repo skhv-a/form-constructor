48cd84b7dc5fc38b7c0dc6f641621f4cba401c69
AlexC <55947391+AlexCards@users.noreply.github.com>
2021-10-06 13:28:49 +0300

    Alert

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,27 @@
+import check from "assets/check.svg";
+import cross from "assets/cross.svg";
+import { joinClassNames } from "utils/utils";
+import "./styles.css";
+
+type AlertType = "success" | "error";
+
+type Props = {
+  type: AlertType;
+  children: React.ReactNode;
+};
+
+const Alert = ({ type, children }: Props) => {
+  return (
+    <div className={joinClassNames("alert", `alert_${type}`)}>
+      <div className="alert__icon">
+        <img
+          src={type === "success" ? check : cross}
+          alt={type === "success" ? "check" : "cross"}
+        />
+      </div>
+      <div className="alert__content">{children}</div>
+    </div>
+  );
+};
+
+export default Alert;
