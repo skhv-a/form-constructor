0121c6bcced41a1c1b94f307f2960a0c57f6a42a
AlexC <55947391+AlexCards@users.noreply.github.com>
2021-10-09 21:34:06 +0300

    /schemas/new page

---
+++ src/pages/schemas/new.tsx
@@ -0,0 +1,19 @@
+import Button from "components/Button/Button";
+import Navbar from "components/Navbar/Navbar";
+import NewSchemaForm from "components/NewSchemaForm/NewSchemaForm";
+
+function NewSchemaPage() {
+  return (
+    <>
+      <Navbar />
+      <div className="content">
+        <NewSchemaForm
+          onSubmit={console.log}
+          controls={<Button>Сохранить схему</Button>}
+        />
+      </div>
+    </>
+  );
+}
+
+export default NewSchemaPage;
