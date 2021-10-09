e9a3977fad655c6a03c1a7d6f2aeb07c58b04eb2
AlexC <55947391+AlexCards@users.noreply.github.com>
2021-10-09 18:09:48 +0300

    NewSchemaForm

---
+++ src/components/NewSchemaForm/NewSchemaForm.tsx
@@ -0,0 +1,86 @@
+import { Schema, SchemaField } from "types/schema";
+import { flatObj } from "utils/utils";
+import Button from "../Button/Button";
+import FieldsAccordion from "../FieldsAccordion/FieldsAccordion";
+import Form from "../Form/Form";
+import Input from "../Input/Input";
+import "./styles.css";
+
+const BLANK_FIELD: SchemaField = {
+  key: "",
+  label: "",
+  type: "string",
+  validation: {},
+  options: [{ key: "", value: "" }],
+};
+
+const INITIAL_VALUES = {
+  name: "",
+  fields: [BLANK_FIELD],
+};
+
+type Props = {
+  initialValues?: Schema;
+  controls: React.ReactNode;
+  onSubmit: (values: Schema) => void;
+};
+
+const NewSchemaForm = ({
+  initialValues = INITIAL_VALUES,
+  controls,
+  onSubmit,
+}: Props) => {
+  return (
+    <Form<Schema> initialValues={initialValues} onSubmit={onSubmit}>
+      {({ errors }) => {
+        const isInvalidField = errors.fields?.some((errs) =>
+          Object.values(flatObj(errs)).some((e) => !!e)
+        );
+
+        return (
+          <>
+            <Form.Item
+              name="name"
+              label="Название схемы"
+              className="schema-name"
+              schema={{ type: "string", required: true }}
+            >
+              <Input />
+            </Form.Item>
+            <div className="divider"></div>
+            <div className="schema-fields">
+              <h2 className="heading-2">Свойства схемы</h2>
+              <p className="schema-fields__subtitle">
+                Схема должна содержать хотя бы одно свойство
+              </p>
+              <Form.List<SchemaField> name="fields">
+                {({ items, push, remove }) => (
+                  <>
+                    <FieldsAccordion
+                      fields={items}
+                      removeField={remove}
+                      className="schema-fields__accordion"
+                    />
+                    <div className="schema-fields__controls">
+                      <Button
+                        htmlType="button"
+                        type="secondary"
+                        disabled={isInvalidField}
+                        onClick={() => push(BLANK_FIELD)}
+                      >
+                        Добавить новое свойство
+                      </Button>
+                      {controls}
+                    </div>
+                  </>
+                )}
+              </Form.List>
+            </div>
+          </>
+        );
+      }}
+    </Form>
+  );
+};
+
+export default NewSchemaForm;
