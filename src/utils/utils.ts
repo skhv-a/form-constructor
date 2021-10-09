18605f3917355450a19431306696e9cf94799c2c
AlexC <55947391+AlexCards@users.noreply.github.com>
2021-10-09 17:54:44 +0300

    new utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,3 +6,25 @@ type RawClassName = string | number | Falsy;
 
 export const joinClassNames = (...classNames: RawClassName[]): string =>
   withoutFalsy(classNames).join(" ");
+
+export const isEmpty = <T>(arr: T[]): boolean => !!arr.length;
+
+export const flatObj = (
+  obj: Record<string, any>,
+  parent: string | null = null,
+  res: Record<string, any> = {}
+) => {
+  for (let key in obj) {
+    let propName = parent
+      ? Array.isArray(obj)
+        ? `${parent}[${key}]`
+        : `${parent}.${key}`
+      : key;
+    if (typeof obj[key] === "object") {
+      flatObj(obj[key], propName, res);
+    } else {
+      res[propName] = obj[key];
+    }
+  }
+  return res;
+};
