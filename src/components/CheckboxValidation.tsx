468e953d8b003d47da8ad56d2b45a664a92d6159
AlexC <55947391+AlexCards@users.noreply.github.com>
2021-10-09 18:02:13 +0300

    CheckboxValidation

---
+++ src/components/CheckboxValidation.tsx
@@ -0,0 +1,17 @@
+import { ValidationItems } from "components/SchemaField/SchemaField";
+import Form from "./Form/Form";
+import Switch from "./Switch/Switch";
+
+const CheckboxValidation: ValidationItems = ({ basePath }) => {
+  return (
+    <Form.Item
+      name={`${basePath}.required`}
+      valuePropName="checked"
+      trigger="onCheck"
+    >
+      <Switch label="Обязательно для заполнения" />
+    </Form.Item>
+  );
+};
+
+export default CheckboxValidation;
