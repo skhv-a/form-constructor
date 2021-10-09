31bb8389fa2027adf71f80d4458c8eab4e772460
AlexC <55947391+AlexCards@users.noreply.github.com>
2021-10-09 21:34:24 +0300

    base /schemas page

---
+++ src/pages/schemas/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "components/Navbar/Navbar";
+
+function SchemasPage() {
+  return (
+    <>
+      <Navbar />
+      <div className="content">Schemas</div>
+    </>
+  );
+}
+
+export default SchemasPage;
