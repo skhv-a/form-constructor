57a86f78aed220d13a2a3d003e0c403fc884ffd5
AlexC <55947391+AlexCards@users.noreply.github.com>
2021-10-10 15:45:48 +0300

    FormFromSchema

---
+++ src/components/schemas[id]/FormFromSchema.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from "react";
+import { Schema } from "types/schema";
+import { FormErrors } from "hooks/useForm";
+import Form from "components/Form/Form";
+import FieldFromSchema from "./FieldFromSchema";
+
+type Props = {
+  schema: Schema;
+  controls: ReactNode;
+  onSubmit: (values: any) => void;
+  onFail: (errors: FormErrors<any>) => void;
+};
+
+const FormFromSchema = ({ schema, controls, onSubmit, onFail }: Props) => {
+  const { fields } = schema;
+
+  const initialValues: any = fields.reduce((acc, field) => {
+    acc[field.key] = undefined;
+    return acc;
+  }, {} as any);
+
+  return (
+    <Form<any>
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      onFail={onFail}
+    >
+      {() => (
+        <>
+          {fields.map((field) => (
+            <FieldFromSchema key={field.key} field={field} />
+          ))}
+          {controls}
+        </>
+      )}
+    </Form>
+  );
+};
+
+export default FormFromSchema;
