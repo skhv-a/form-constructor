6e79165f179fe838c3b6b3d53a2ab3acaea72056
AlexC <55947391+AlexCards@users.noreply.github.com>
2021-10-11 09:26:13 +0300

    Updates

--- src/utils/token.ts
+++
@@ -1,6 +0,0 @@
-const TOKEN_KEY = "token";
-
-export const getUserToken = (): string => localStorage.getItem(TOKEN_KEY) ?? "";
-
-export const setUserToken = (token: string): void =>
-  localStorage.setItem(TOKEN_KEY, token);
